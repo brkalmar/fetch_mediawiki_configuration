ccd9deaa63089b9c6fc6f44f9c0ec5465587564d
Bence Kalmar <bkalmar1996@gmail.com>
2021-10-18 00:17:31 +0200

    feat: implement create API URL and fetch response

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ fn run() -> Result<(), Box<dyn error::Error>> {
             .unwrap_or(simplelog::LevelFilter::Info),
         simplelog::ConfigBuilder::default()
             .set_level_padding(simplelog::LevelPadding::Left)
+            .set_thread_level(simplelog::LevelFilter::Trace)
             .set_thread_mode(simplelog::ThreadLogMode::Both)
             .build(),
         simplelog::TerminalMode::Stderr,
@@ -47,9 +48,67 @@ Maximum log level can be set in env variable `{}` to one of `off`, `error`, `war
                 .required(true),
         )
         .get_matches();
-    let domain = clap::value_t!(matches.value_of("domain"), String).unwrap_or_else(|e| e.exit());
 
+    let domain = clap::value_t!(matches.value_of("domain"), String).unwrap_or_else(|e| e.exit());
     log::info!("wiki domain: {:?}", domain);
+
+    let url = {
+        const CATEGORIES: &[&str] = &[
+            "extensiontags",
+            "general",
+            "magicwords",
+            "namespacealiases",
+            "namespaces",
+            "protocols",
+        ];
+        let mut url = reqwest::Url::parse_with_params(
+            "https://example.org/w/api.php",
+            [
+                ("action", "query"),
+                ("meta", "siteinfo"),
+                ("siprop", &CATEGORIES.join("|")),
+                ("format", "json"),
+                ("formatversion", "2"),
+                ("errorformat", "plaintext"),
+            ],
+        )
+        .unwrap();
+        url.set_host(Some(&domain))?;
+        url
+    };
+    log::debug!("url = {}", url);
+
+    let user_agent = format!(
+        "{}/{} ({})",
+        clap::crate_name!(),
+        clap::crate_version!(),
+        clap::crate_authors!(", ")
+    );
+    log::debug!("user_agent = {:?}", user_agent);
+    let client = reqwest::blocking::Client::builder()
+        .user_agent(user_agent)
+        .https_only(true)
+        .deflate(true)
+        .gzip(true)
+        .build()?;
+
+    let response = client.get(url).send()?.error_for_status()?;
+    log::info!("response status: {}", response.status());
+    let log_header = |name| {
+        log::debug!(
+            "response header: {:?}: {:?}",
+            name,
+            response.headers().get(&name)
+        );
+    };
+    log_header(reqwest::header::CONNECTION);
+    log_header(reqwest::header::CONTENT_ENCODING);
+    log_header(reqwest::header::CONTENT_LENGTH);
+    log_header(reqwest::header::CONTENT_TYPE);
+    log_header(reqwest::header::SERVER);
+    log_header(reqwest::header::HeaderName::from_static(
+        "mediawiki-api-error",
+    ));
 
     todo!()
 }
